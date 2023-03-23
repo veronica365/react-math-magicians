556d8f571c0ebac8d24a269063eb33b5e1678e23
veronica365 <naruth10@gmail.com>
2023-03-23 19:20:44 +0300

    Add useEffect, state, props and add the quote to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Calculator } from './components/Calculator';
+import fetchQuotes from './logic/fetchQuotes';
+import Quote from './components/Quote';
+import ErrorOrLoading from './components/ErrorOrLoading';
 
 function App() {
-  return <Calculator />;
+  const [state, setQuotes] = useState({ loading: true });
+  useEffect(() => {
+    const getResults = async () => {
+      const [result] = await (await fetchQuotes()).json();
+      if (result) return setQuotes(result);
+      return setQuotes({ loading: null });
+    };
+    getResults();
+  }, []);
+  return (
+    <div className="wrapper">
+      <ErrorOrLoading loading={state.loading} error={state?.loading === null} />
+      {state.author && <Quote quote={state.quote} author={state.author} />}
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
